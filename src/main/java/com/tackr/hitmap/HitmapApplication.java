bb603409282bbf32057dd12896145b5640f816d9
Thayna Caixeta <thayna.caixeta@gmail.com>
2025-06-14 23:58:36 +0100

    Generate Project Using Spring Initializr

---
+++ src/main/java/com/tackr/hitmap/HitmapApplication.java
@@ -0,0 +1,13 @@
+package com.tackr.hitmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tackr/hitmap/HitmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tackr.hitmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
